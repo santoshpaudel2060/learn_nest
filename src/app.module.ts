65c7974583a522cfaadcffc179d65b44f9508b7b
santosh paudel <santoshppaudel098@gmail.com>
2026-08-29 14:17:23 +0545

    project setup

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { AuthModule } from './auth/auth.module.js';
+
+export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
+@Module({
+  imports: [AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
